c1b2153847158daa240488be837805eac3301670
Destiny Samuel <destinysamuel37@gmail.com>
2019-10-02 04:01:14 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  puts "hi"
+num1+num2
 end
 
 def subtraction(num1, num2)
-
+puts "sup"
+num1-num2
 end
 
 def division(num1, num2)
-
+puts "nothing much"
+num1/num2
 end
 
 def multiplication(num1, num2)
-
+puts "hbu"
+num1*num2
 end
 
 def modulo(num1, num2)
-
+  puts "chillin"
+  num1 % num2
 end
 
 def square_root(num)
-
+puts "nice"
+Math.sqrt(num)
 end
